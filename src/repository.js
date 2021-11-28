dade2b9acabed79f49a13402068742e367bc8e91
Bruno Ilponse <bruno.ilponse@gmail.com>
2021-11-28 06:23:08 +0100

    Renamming

--- src/repository.js
+++ src/repository.js
@@ -4,9 +4,7 @@ const config = require('./config');
 module.exports = class Repository {
 	constructor(repository, workdir) {
 	    this.repository = repository;
-	    this.tmp_dir = `${workdir}/discord-lingui-tmp`;
-	    this.repo_dir = `${workdir}/discord-lingui-repo`;
-
+	    this.workdir = `${workdir}`;
 	}
 	async run_script(command, args, options) {
 		console.log(command);
@@ -28,16 +26,17 @@ module.exports = class Repository {
 	}
 	async get_stats(msg) {
 	    try {
-	        await this.run_script('init_repo', [ this.tmp_dir, this.repository ]);
-	        await this.run_script('update_repo', [ this.tmp_dir ]);
+	        await this.run_script('repo_init', [ this.workdir, this.repository ]);
+	        await this.run_script('repo_update', [ this.workdir ]);
 
-	        const frontend_commit = await this.run_script("git_get_commit", [ this.tmp_dir, "" ]);
-	        const translations_commit = await this.run_script("git_get_commit", [ this.tmp_dir, "/src/locales/translations" ]);
+	        const frontend_commit = await this.run_script("git_get_commit", [ this.workdir, "" ]);
+	        const translations_commit = await this.run_script("git_get_commit", [ this.workdir, "/src/locales/translations" ]);
 	        
 	        let output = config.get("extraction_stats");
 
 	        if (frontend_commit != config.get("frontend_commit") || translations_commit != config.get("translations_commit")) {
-		        output = await this.run_script('extract_translations', [ this.tmp_dir ]);
+	        	await this.run_script('repo_install', [ this.workdir ]);
+		        output = await this.run_script('extract_translations', [ this.workdir ]);
 		        config.set("extraction_stats", output);
 		        config.set("frontend_commit", frontend_commit);
 		        config.set("translations_commit", translations_commit);
